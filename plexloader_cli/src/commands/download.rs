fd70d341a76fa5d599149a2db63cd5527493d110
warwicklol14 <githubta@kratos59.33mail.com>
2022-07-06 19:56:34 -0400

    Better error handling in commands

--- plexloader_cli/src/commands/download.rs
+++ plexloader_cli/src/commands/download.rs
@@ -17,12 +17,12 @@ pub struct Download {
 impl CommandHandler for Download {
     fn handle(self: &Self) -> Result<()> {
         let plex_user = deserialize_plex_user()
-            .with_context(|| "Unable to use previous auth. Maybe try logging in again?")?;
+            .with_context(|| "unable to use previous auth. try logging in again?")?;
         let plex_loader = PlexLoader::new(plex_user);
         let req_media_metadata_uri = plex_loader.get_metadata_uri(&self.link)
-            .with_context(|| "Unable to parse metadata uri")?;
+            .with_context(|| "unable to parse metadata uri")?;
         let req_media_container = plex_loader.get_media(&req_media_metadata_uri)
-            .with_context(|| "Unable to deserialize media container")?;
+            .with_context(|| "unable to deserialize media container")?;
         println!("{:?}", req_media_container);
         Ok(())
     }
--- plexloader_cli/src/commands/login.rs
+++ plexloader_cli/src/commands/login.rs
@@ -1,7 +1,8 @@
 use clap::{Args};
 
 use plexloader_lib::loader::login::plex_login_through_credentials;
-use plexloader_lib::NetworkResponseError;
+
+use anyhow::{Context};
 
 use crate::utils::serialize_plex_user;
 use super::CommandHandler;
@@ -17,18 +18,12 @@ pub struct Login {
     password: String,
 }
 
-use thiserror::Error;
-
-#[derive(Error, Debug)]
-pub enum LoginCommandError{
-    #[error("Error: net {0}")]
-    NetworkResponseError(#[from] NetworkResponseError),
-}
-
 impl CommandHandler for Login {
     fn handle(self: &Self) -> anyhow::Result<()> {
-        let plex_user = plex_login_through_credentials(&self.username, &self.password)?;
-        serialize_plex_user(plex_user)?;
+        let plex_user = plex_login_through_credentials(&self.username, &self.password)
+            .with_context(|| "unable to to login. check your credentials")?;
+        serialize_plex_user(plex_user)
+            .with_context(|| "unable to save auth info")?;
         println!("Login was successfull");
         Ok(())
     }
