96c20ec7009e22e9e1185aeaebcc5a65dccd71dc
warwicklol14 <githubta@kratos59.33mail.com>
2022-07-13 03:04:38 -0400

    Changed login from options to Input

--- plexloader_cli/src/commands/login.rs
+++ plexloader_cli/src/commands/login.rs
@@ -2,25 +2,28 @@ use clap::{Args};
 
 use plexloader_lib::loader::login::plex_login_through_credentials;
 
+use dialoguer::{
+    Input,
+    theme::ColorfulTheme,
+};
+
 use anyhow::{Context};
 
 use crate::utils::{serialize_plex_user, success};
 use super::CommandHandler;
 
 #[derive(Args)]
-pub struct Login {
-    /// Plex username
-    #[clap(short, long)]
-    username: String,
-
-    /// Plex password
-    #[clap(short, long)]
-    password: String,
-}
+pub struct Login {}
 
 impl CommandHandler for Login {
     fn handle(self: &Self) -> anyhow::Result<()> {
-        let plex_user = plex_login_through_credentials(&self.username, &self.password)
+        let username: String = Input::with_theme(&ColorfulTheme::default())
+            .with_prompt("Your plex username")
+            .interact()?;
+        let password: String = Input::with_theme(&ColorfulTheme::default())
+            .with_prompt("Your plex password")
+            .interact()?;
+        let plex_user = plex_login_through_credentials(&username, &password)
             .with_context(|| "unable to to login. check your credentials")?;
         serialize_plex_user(plex_user)
             .with_context(|| "unable to save auth info")?;
